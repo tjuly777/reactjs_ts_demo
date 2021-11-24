0984ef2efbd2a0f45ef50d4d54b42ccda97a89bc
tjuly777 <tjuly777@gmail.com>
2021-11-24 15:28:02 +0900

    ref:typeScriptへの書き換えを途中まで

--- src/index.tsx
+++ src/index.tsx
@@ -1,57 +1,117 @@
 import React from 'react'
+import { useState } from 'react'
+import { Repeat } from 'typescript-tuple'
 import ReactDOM from 'react-dom'
 import './index.css'
-class Square extends React.Component {
-  render() {
-    return <button className="square">{/* TODO */}</button>
-  }
+
+type SquareState = 'X' | 'O' | null
+
+interface SquareProps {
+  value: SquareState
+  onClick: () => void
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />
-  }
+const Square = (props: SquareProps) => (
+  <button className="square" onClick={() => props.onClick}>
+    {props.value}
+  </button>
+)
 
-  render() {
-    const status = 'Next player: X'
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    )
-  }
+type BoardState = Repeat<SquareState, 9>
+type BoardProps = {
+  squares: BoardState
+  onClick: (i: number) => void
 }
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+const Board = (props: BoardProps) => {
+  const renderSquare = (i: number) => (
+    <Square value={props.squares[i]} onClick={() => props.onClick(i)} />
+  )
+  const status = 'Next player: X'
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    )
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  )
+}
+
+type Step = {
+  squares: BoardState
+  xIsNext: boolean
+}
+
+type GameState = {
+  readonly history: Step[]
+  readonly stepNumber: number
+}
+
+const Game = () => {
+  const [state, setState] = useState<GameState>({
+    history: [
+      {
+        squares: [null, null, null, null, null, null, null, null, null],
+        xIsNext: true,
+      },
+    ],
+    stepNumber: 0,
+  })
+
+  const current = state.history[state.stepNumber]
+
+  const handleClick = (i: number) => {
+    const next: Step = (({ squares, xIsNext }) => {
+      const nextSquares = squares.slice() as BoardState
+      nextSquares[i] = xIsNext ? 'X' : 'O'
+      return {
+        squares: nextSquares,
+        xIsNext: !xIsNext,
+      }
+    })(current)
+
+    setState(({ history, stepNumber }) => {
+      const newHistory = history.slice(0, stepNumber + 1).concat(next)
+
+      return {
+        history: newHistory,
+        stepNumber: newHistory.length - 1,
+      }
+    })
   }
+
+  const jumpTo = (move: number) => {
+    setState((prev) => ({
+      ...prev,
+      stepNumber: move,
+    }))
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={current.squares} onClick={() => handleClick(state.stepNumber)} />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  )
 }
 
 // ========================================
